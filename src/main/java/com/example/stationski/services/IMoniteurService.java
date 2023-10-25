9b940e502bb6896f5c5912295f64d119ea909b60
yassinebensalh <yassine.bensalha@sprit.tn>
2023-10-26 00:01:38 +0100

    idk

--- src/main/java/com/example/stationski/services/IMoniteurService.java
+++ src/main/java/com/example/stationski/services/IMoniteurService.java
@@ -2,7 +2,6 @@ package com.example.stationski.services;
 
 import com.example.stationski.entities.Moniteur;
 import com.example.stationski.entities.MoniteurDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
